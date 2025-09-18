d6adc14cc206df69df10f6ba64de94b63e1053a1
Simone Primarosa <simonepri@outlook.com>
2025-09-18 00:06:24 +0000

    Don't block user redirects on google sheet API logging

--- src/routes/gs.js
+++ src/routes/gs.js
@@ -3,6 +3,7 @@ import {Buffer} from 'node:buffer';
 import {google} from 'googleapis';
 import validURL from 'valid-url';
 import {UAParser} from 'ua-parser-js';
+import {waitUntil} from '@vercel/functions';
 
 const pixel = Buffer.from(
   'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR4nGNiYAAAAAkAAxkR2eQAAAAASUVORK5CYII=',
@@ -21,50 +22,46 @@ const auth = new google.auth.GoogleAuth({
 
 const sheets = google.sheets({version: 'v4', auth});
 
-const gsRoute = async (request, response) => {
-  const values = {};
-  values.ac = request.query.a;
-  values.re = request.query.r;
-  values.ts = new Date().toISOString();
-  const ua = new UAParser(request.headers['user-agent']).getResult();
-  values.de = ua.device.type || 'desktop';
-  values.os = ua.os.name;
-  values.br = ua.browser.name;
-  values.ct = request.headers['x-vercel-ip-city'];
-  values.rn = request.headers['x-vercel-ip-country-region'];
-  values.co = request.headers['x-vercel-ip-country'];
+const gsRoute = (request, response) => {
+  const redirectUrl = request.query.r || '';
+  const encodedUrl = encodeURI(redirectUrl);
 
-  const row = ['ts', 'de', 'os', 'br', 'ct', 'rn', 'co', 'ac', 're'].map(
-    (col) => values[col],
-  );
-
-  try {
-    const options = {
-      spreadsheetId: request.query.id,
-      range: 'A2',
-      valueInputOption: 'RAW',
-      insertDataOption: 'INSERT_ROWS',
-      resource: {values: [row]},
-    };
-    await sheets.spreadsheets.values.append(options);
-  } catch (error) {
-    if (!['INVALID_ARGUMENT', 'NOT_FOUND'].includes(error.status)) {
-      console.error(error);
-    }
-  }
-
-  const euri = encodeURI(request.query.r);
-  if (validURL.isUri(euri)) {
-    // Redirect
-    response.statusCode = 302;
-    response.setHeader('Location', euri);
-    response.end();
+  if (validURL.isUri(encodedUrl)) {
+    response.writeHead(302, {Location: encodedUrl}).end();
   } else {
-    // Show pixel
-    response.statusCode = 200;
-    response.setHeader('content-type', 'image/png');
-    response.end(pixel);
+    response.writeHead(200, {'Content-Type': 'image/png'}).end(pixel);
   }
+
+  const ua = new UAParser(request.headers['user-agent']).getResult();
+
+  const row = [
+    /* Timestamp */ new Date().toISOString(),
+    /* Device */ ua.device.type || 'desktop',
+    /* OS */ ua.os.name || '',
+    /* Browser */ ua.browser.name || '',
+    /* City */ request.headers['x-vercel-ip-city'] || '',
+    /* Region */ request.headers['x-vercel-ip-country-region'] || '',
+    /* Country */ request.headers['x-vercel-ip-country'] || '',
+    /* Action Name */ request.query.a || '',
+    /* Redirect URL */ redirectUrl,
+  ];
+  /* eslint-disable promise/prefer-await-to-then */
+  waitUntil(
+    sheets.spreadsheets.values
+      .append({
+        spreadsheetId: request.query.id,
+        range: 'A2',
+        valueInputOption: 'RAW',
+        insertDataOption: 'INSERT_ROWS',
+        resource: {values: [row]},
+      })
+      .catch((error) => {
+        if (!['INVALID_ARGUMENT', 'NOT_FOUND'].includes(error.status)) {
+          console.error(error);
+        }
+      }),
+  );
+  /* eslint-enable promise/prefer-await-to-then */
 };
 
 export default gsRoute;
